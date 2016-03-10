23f4da7ca01d2fc2c947449d0989c6a29182a13b
arebe <rfboyce@gmail.com>
2016-03-09 22:06:29 -0500

    slider, branch section

--- js/app.js
+++ js/app.js
@@ -1,17 +1,8 @@
-$(document).foundation({
-  orbit: {
-    animation: 'slide',
-    timer_speed: 1000,
-    pause_on_hover: true,
-    animation_speed: 500,
-    navigation_arrows: true,
-    bullets: false
-  }
-});
+$(document).foundation();
 
 $(document).ready(function() {
-
-	// animate to top of trunk - turned off for dev
+  console.log("hi there")
+	//animate to top of trunk - turned off for dev
 	$('html, body').animate({
 		scrollTop: $("#trunk").offset().top
 	}, 1000)
